1a161c558cbbf2b3388768686127d4a6e05cd0d6
Elizabeth Sanger <libsang@gmail.com>
2016-07-26 09:11:07 -0500

    outputting songs to DOM

--- songs.js
+++ songs.js
@@ -6,12 +6,22 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-console.log(songs);
-
+// console.log(songs);
+// console.log(songs.join(','));
+console.log(songs.join(','));
 
 songs.forEach(outputSongs);
 
+// //TAKE JOIN OUTSIDE OF LOOP//THIS IS YOUR ISSUE
 
-function outputSongs(powieht) {
-    console.log(powieht);
+function outputSongs(song) {
+
+    // var updated_songs = song.join(',');
+    songs = song.replace('>', '-').replace(/[@/\(/!/\*/]/g,'');
+
+    document.getElementById("songbox").innerText += songs;
+    console.log(songs);
 }
+
+
+songs.
