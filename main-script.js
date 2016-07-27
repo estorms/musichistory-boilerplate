9114fe1158399e390326973cf08e115fb3d2e573
Elizabeth Sanger <libsang@gmail.com>
2016-07-27 18:28:13 -0500

    updating to json format, changed file names for clarity

---
+++ main-script.js
@@ -0,0 +1,13 @@
+console.log('shoppers')
+
+function showSongs1(songs1) {
+  console.log(songs1);
+}
+
+music.getSongs1(showSongs1)
+
+function showSongs2(songs2) {
+  console.log(songs2);
+}
+
+music.getSongs2(showSongs2)
